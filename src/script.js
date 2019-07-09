88e2a0cc9642319cf52451d31e2e077f586e2b5a
fontoful <40501207+fontoful@users.noreply.github.com>
2019-07-08 18:33:01 -0600

    added some js

---
+++ src/script.js
@@ -0,0 +1,38 @@
+const leftArrow = document.querySelector("#left-arrow");
+
+const activeDisplay = document.querySelectorAll(".display");
+
+const rightArrow = document.querySelector("#right-arrow");
+
+// functions
+const slideRight = () => {
+  //   get current display item
+  const currentActive = document.querySelector(".active");
+
+  //   remove active class
+  currentActive.classList.remove("active");
+
+  if (currentActive.nextElementSibling) {
+    currentActive.nextElementSibling.classList.add("active");
+  } else {
+    activeDisplay[0].classList.add("active");
+  }
+};
+
+const slideLeft = () => {
+  // get current class with active
+  const currentActive = document.querySelector(".active");
+  // remove active class
+  currentActive.classList.remove("active");
+
+  if (currentActive.previousElementSibling) {
+    currentActive.previousElementSibling.classList.add("active");
+  } else {
+    activeDisplay[activeDisplay.length - 1].classList.add("active");
+  }
+};
+// event listener
+
+rightArrow.addEventListener("click", slideRight);
+
+leftArrow.addEventListener("click", slideLeft);
