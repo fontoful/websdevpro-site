0ddd75f9efd96edb97d925fb8f0de3d8eab2e781
fontoful <40501207+fontoful@users.noreply.github.com>
2019-07-19 17:48:51 -0600

    actual text aded

--- src/script.js
+++ src/script.js
@@ -6,30 +6,30 @@ const rightArrow = document.querySelector("#right-arrow");
 
 // functions
 const slideRight = () => {
-  //   get current display item
-  const currentActive = document.querySelector(".active");
+    //   get current display item
+    const currentActive = document.querySelector(".active");
 
-  //   remove active class
-  currentActive.classList.remove("active");
+    //   remove active class
+    currentActive.classList.remove("active");
 
-  if (currentActive.nextElementSibling) {
-    currentActive.nextElementSibling.classList.add("active");
-  } else {
-    activeDisplay[0].classList.add("active");
-  }
+    if (currentActive.nextElementSibling) {
+        currentActive.nextElementSibling.classList.add("active");
+    } else {
+        activeDisplay[0].classList.add("active");
+    }
 };
 
 const slideLeft = () => {
-  // get current class with active
-  const currentActive = document.querySelector(".active");
-  // remove active class
-  currentActive.classList.remove("active");
+    // get current class with active
+    const currentActive = document.querySelector(".active");
+    // remove active class
+    currentActive.classList.remove("active");
 
-  if (currentActive.previousElementSibling) {
-    currentActive.previousElementSibling.classList.add("active");
-  } else {
-    activeDisplay[activeDisplay.length - 1].classList.add("active");
-  }
+    if (currentActive.previousElementSibling) {
+        currentActive.previousElementSibling.classList.add("active");
+    } else {
+        activeDisplay[activeDisplay.length - 1].classList.add("active");
+    }
 };
 // event listener
 
@@ -39,5 +39,5 @@ leftArrow.addEventListener("click", slideLeft);
 
 // make it slid every 3 seconds
 setInterval(() => {
-  slideRight();
+    slideRight();
 }, 6000);
